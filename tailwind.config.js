6bb127c3aad3e4881f096b409947b58c20caabe3
ISMET KAHYA <ikahya@st.noorderpoort.nl>
2026-01-15 15:33:56 +0100

    .

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        iceland: ["Iceland", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
